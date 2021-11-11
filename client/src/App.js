76124bf56b99b7bb8d4880162a298aecee8bdf72
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-11-10 19:51:44 -0800

    done with mvp

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import MovieHeader from "./components/MovieHeader";
 
 import EditMovieForm from "./components/EditMovieForm";
 import FavoriteMovieList from "./components/FavoriteMovieList";
+import AddMovieForm from "./components/AddMovieForm";
 
 import axios from "axios";
 
@@ -57,6 +58,10 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies} />
 
           <Switch>
+            <Route path="/addmovie">
+              <AddMovieForm setMovies={setMovies} />
+            </Route>
+
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies} />
             </Route>
